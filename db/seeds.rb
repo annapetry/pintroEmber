9a4af77ad7da16867f7f7b27ee4da2bafdf21f67
Anna Petry <hello@annapetry.com>
2015-05-04 09:44:16 -0700

    WIP user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+case Rails.env
+when 'development'
+  names = [
+    ["Anna", "Petry"],
+    ["John", "Mogielnicki"],
+    ["Kate", "Harrington-Rosen"],
+    ["Jessi", "Mons"]
+  ]
+
+  names.each_with_index do |name, i|
+    first_name = name[0]
+    last_name  = name[1]
+
+    email ="#{first_name.downcase}.#{last_name.downcase}@art.com"
+    binding.pry
+    User.create {{ username: '#{first_name.downcase}', first_name: first_name, last_name: last_name, password: 'password',
+      location: 'SF, CA', description: 'Cutie ##{i}', email: email, avatar_url: 'http://www.annapetry.com/assets/petry.jpg' }}
+  end
+end
